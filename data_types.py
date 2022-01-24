5b3ee403d50e319bed9a7226dc7f6f1579c7a951
andrew irvine <andrewirvine3381@gmail.com>
2022-01-24 12:46:29 +0000

    adds more files to repository

---
+++ data_types.py
@@ -0,0 +1,27 @@
+# Assigns variables to data
+var1 = "andrew"
+var1a = 'andrew'
+var2 = 50.0
+var3 = True
+# Changes the float to an integer or a string as a variable
+var4 = int(var2)
+var5 = str(var2)
+# Will print to the screen
+print('Hello')
+# Shows what functions cant be run and the correct way torun them
+# Concatenate
+print(var1 + var1a)
+# Sum 
+#print(var1 + var2)
+# Concatented
+print(var1 , var2)
+#concat with data type change
+print (var1 + str(var2))
+# F-string
+print(f"I like{var1}{var2}")
+# Shows use of the escape character \
+var6 = 'Hello my name is \"Andrew\"'
+print(var6)
+# Sows useof the new line command \\n
+var7 = "I want text \\n across two lines"
+print(var7)
