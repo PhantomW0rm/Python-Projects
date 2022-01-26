4e82bd82f44dc2bc8c7dcf8d6bef6fe1fc57c2c7
andrew irvine <andrewirvine3381@gmail.com>
2022-01-26 10:31:42 +0000

    adds the debugging files

---
+++ debugging_exercises.py
@@ -0,0 +1,17 @@
+#user_funds = 10.31
+#item_price = 10.00
+
+#if item_price < user_funds:
+#    print("You have enough money!")
+#if item_price == user_funds:
+#    print("You have the precise amount of money")
+#if item_price > user_funds:
+#    print("Sorry you don't have enough money")
+
+def product(n):
+        total == 1
+for n in n:
+        total *= i
+print(total)
+
+print(product([4,4,5]))
